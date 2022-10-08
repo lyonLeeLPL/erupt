34ed3829b3da7c5edacff1bef21887425ca5bea4
a5601 <474857849@qq.com>
2022-10-08 14:05:06 +0900

    erupt common

---
+++ erupt-common/src/main/java/xyz/erupt/common/constant/CommonConst.java
@@ -0,0 +1,11 @@
+package xyz.erupt.common.constant;
+
+/**
+ * @author YuePeng
+ * date 2019-07-05.
+ */
+public class CommonConst {
+
+    public static final String SPECIAL_SPLIT_SYMBOL = "_X_";
+
+}

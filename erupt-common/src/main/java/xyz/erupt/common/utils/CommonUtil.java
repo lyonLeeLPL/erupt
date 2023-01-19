54aafee3e1a34b7527bac27f7f0652b4b4a2837b
lipihnlioang <lipihnlioang@intasect.co.jp>
2023-01-19 12:55:46 +0900

    maven fix new one v2

---
+++ erupt-common/src/main/java/xyz/erupt/common/utils/CommonUtil.java
@@ -0,0 +1,20 @@
+package xyz.erupt.common.utils;
+
+import cn.hutool.core.io.file.PathUtil;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Objects;
+
+/**
+ * @author YuePeng
+ * date 2019-07-05.
+ */
+public class CommonUtil {
+
+    public static<T> boolean isStartupFromJar(Class<T> clazz) {
+        File file = new File(clazz.getProtectionDomain().getCodeSource().getLocation().getPath());
+        return file.isFile();
+    }
+
+}
